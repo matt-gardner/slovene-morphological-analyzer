b9a3e86a5e391868ab800101def8093d02532ce9
Matt Gardner <mjg82@byu.edu>
2013-02-28 11:08:32 -0500

    Starting on adjectives, but needs more work.  27 percent.

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def main(lexica, foma_file, test_files, results_dir, verbose):
         # This randomizes the order, which is good, if you want to just pick
         # the first N, or something
         keys = list(cases.keys())
-        while start+blocksize < len(keys):
+        while start == 0 or start+blocksize < len(keys):
             if start % 20000 == 0:
                 print start
             block = keys[start:start+blocksize]
@@ -43,11 +43,12 @@ def main(lexica, foma_file, test_files, results_dir, verbose):
             proc.stdout.close()
             start += blocksize
 
-        # TODO: test-specific error files?
-        incorrect_file = create_dirs_and_open(results_dir + 'incorrect.txt')
-        correct_file = create_dirs_and_open(results_dir + 'correct.txt')
+        base = results_dir + test.split('/')[-1][:-4] + '_'
+        incorrect_file = create_dirs_and_open(base + 'incorrect.txt')
+        correct_file = create_dirs_and_open(base + 'correct.txt')
         num_correct = 0
         num_incorrect = 0
+        num_complete_recall = 0
         incorrect_lemmas = set()
         for form in cases:
             gold_set = cases[form]
@@ -70,25 +71,36 @@ def main(lexica, foma_file, test_files, results_dir, verbose):
             else:
                 correct_file.write('%s\n' % form)
                 num_correct += 1
+            if recall == 1.0:
+                num_complete_recall += 1
         incorrect_file.close()
         correct_file.close()
-        total = num_correct + num_incorrect
+        total = len(cases)
         print 'Forms tested:', total
-        print 'Percent correct:', num_correct / total
-        incorrect_lemma_file = open(results_dir + 'incorrect_lemmas.txt', 'w')
+        print 'Percent completely correct:', num_correct / total
+        print 'Percent with 100% recall:', num_complete_recall / total
+        incorrect_lemma_file = open(base + 'incorrect_lemmas.txt', 'w')
         for lemma in incorrect_lemmas:
             incorrect_lemma_file.write('%s\n' % lemma)
         incorrect_lemma_file.close()
 
 
 def analysis_to_msd(analysis):
-    replacements = [('+Nom', 'n'), ('+Gen', 'g'), ('+Dat', 'd'), ('+Acc', 'a'),
+    # The order of these is important.  Some symbols are prefixes of other
+    # symbols, so the longer symbols must be placed first.  I'm grouping them
+    # by phenomenon, so hopefully it will be easier to spot.
+    replacements = [
+            ('+General', 'g'), ('+Possessive', 's'), ('+Participle', 'p'),
+            ('+Positive', 'p'), ('+Comparative', 'c'), ('+Superlative', 's'),
+            ('+Animate', 'y'), ('+Inanimate', 'n'),
+            ('+Definite', 'y'), ('+Indefinite', 'n'),
+            ('+Nom', 'n'), ('+Gen', 'g'), ('+Dat', 'd'), ('+Acc', 'a'),
             ('+Loc', 'l'), ('+Ins', 'i'),
             ('+Sing', 's'), ('+Dual', 'd'), ('+Plural', 'p'),
             ('+Masc', 'm'), ('+Fem', 'f'), ('+Neut', 'n'),
-            ('+Animate', 'y'), ('+Inanimate', 'n'),
+            ('+A', '-A'),
+            ('+N', '-Nc'), # Not general, yet; still need to handle propers
             ]
-    replacements.append(('+N', '-Nc')) # not general, yet
     msd = analysis
     for a, m in replacements:
         msd = msd.replace(a, m)
@@ -109,19 +121,22 @@ if __name__ == '__main__':
     opts, args = parser.parse_args()
     lexica = [
             'lexica/base.lexc',
+            'lexica/adjs.lexc',
+            'lexica/adj_rules.lexc',
             'lexica/common_fem_nouns.lexc',
             'lexica/common_masc_nouns.lexc',
             'lexica/common_neut_nouns.lexc',
             'lexica/noun_rules.lexc',
             ]
-    foma_file = 'foma/nouns.foma'
+    foma_file = 'foma/slovene.foma'
     if opts.small:
-        test_files = ['tests/nouns_small.tsv']
+        test_files = ['tests/adjs_small.tsv']
     else:
         test_files = [
-                'tests/common_fem_nouns.tsv',
-                'tests/common_masc_nouns.tsv',
-                'tests/common_neut_nouns.tsv',
+                #'tests/common_fem_nouns.tsv',
+                #'tests/common_masc_nouns.tsv',
+                #'tests/common_neut_nouns.tsv',
+                'tests/adjs.tsv',
                 ]
     results_dir = 'results/'
     main(lexica, foma_file, test_files, results_dir, opts.verbose)
