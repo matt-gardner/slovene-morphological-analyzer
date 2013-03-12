7eae2d0f232513ba05753b2cc439668316e7ad74
Matt Gardner <mjg82@byu.edu>
2013-03-11 21:41:39 -0400

    Added a script to help locate errors

---
+++ find_errors.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from data.create_tests import SEPARATOR
+
+def main(pos, query_msd):
+    results_file = 'results/%s_incorrect.txt' % pos
+    test_file = 'tests/%s.tsv' % pos
+    out_file = 'results/%s_incorrect.txt' % query_msd
+    out = open(out_file, 'w')
+    incorrect = set()
+    for line in open(results_file):
+        incorrect.add(line.strip())
+    for line in open(test_file):
+        form, analyses = line.strip().split('\t')
+        for analysis in analyses.split(SEPARATOR):
+            lemma, msd = analysis.split('-', 1)
+            if msd == query_msd and form in incorrect:
+                print form
+                out.write('%s\n' % form)
+    out.close()
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('', '--pos',
+            dest='pos',
+            help='Part of speech to store',
+            )
+    parser.add_option('', '--msd',
+            dest='msd',
+            help='MSD to look for',
+            )
+    opts, args = parser.parse_args()
+    main(opts.pos, opts.msd)
+
+# vim: et sw=4 sts=4
